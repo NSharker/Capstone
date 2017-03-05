58f33a7929f75cfa34b017d3b1cbf0df7eefe9ad
Andres Quinones <aquinones340@hotmail.com>
2017-03-05 13:19:17 -0500

    Added Taxi Data Parser

--- Utility Tools/Parsers/parser.py
+++ Utility Tools/Parsers/parser.py
@@ -53,10 +53,31 @@ def complaintDataParser(inputFileName, outputFileName):
 
 
 
+'''
+tpep_pickup_datetime
+tpep_dropoff_datetime
+Pickup_longitude
+Pickup_latitude
+Dropoff_longitude
+Dropoff_ latitude
+Passenger_count'''
+
+def taxiDataParser(inputFileName, outputFileName):
+    with open(inputFileName, 'r') as csvfile:
+        with open(outputFileName, 'w') as writecsvfile:
+            complaintData = csv.DictReader(csvfile, delimiter=',')
+            outputCSV = csv.writer(writecsvfile, delimiter=',')
+            outputCSV.writerow(['pickup_datetime','dropoff_datetime','pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude','passenger_count'])
+            for row in complaintData:
+                outRow = [row['pickup_datetime'], row['dropoff_datetime'], row['pickup_longitude'], row['pickup_latitude'], row['dropoff_longitude'], row['dropoff_latitude'], row['passenger_count']]
+                outputCSV.writerow(outRow)
+
         
 
-complaintDataParser("NYPD_Complaint_Data_Historic.csv","output.csv")
-        
+#complaintDataParser("NYPD_Complaint_Data_Historic.csv","output.csv")
+
+
+taxiDataParser("yellow_tripdata_2010-01.csv", "yellow2010output.csv")
         
         
         
