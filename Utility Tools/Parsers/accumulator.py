70907e1f8503198e062c314b40e85aadd1546217
NSharker <nishad.sharker@gmail.com>
2017-03-30 10:29:42 -0400

    Added Complaint Accumulator
    -Added the function for the Complaint Accumulator
    -All accumulation functions are done

--- Utility Tools/Parsers/accumulator.py
+++ Utility Tools/Parsers/accumulator.py
@@ -72,7 +72,131 @@ def taxiDataAccumulator(inputFileName, outputFileName):
                 outPutCSV.writerow([date,zipCode,pickUpNumber, dropOffNumber, pickUpPassengerNumber, dropOffPassengerNumber])
             
                 
+
+
+
+def complaintDataAccumulator(inputFileName, outputFileName):
+    with open(inputFileName, 'r') as csvfile:
+        with open(outputFileName, 'w', newline = '') as outfile:
+            complaintData = csv.DictReader(csvfile, delimiter = ',')
+            #dictionaries with keys = zipcode, and values = accumulated number of events for that month
+            felonyAccumulation = {}
+            misdemeanorAccumulation = {}
+            violationAccumulation = {}
+            firstRow = next(complaintData)
+            #Use these two variables to check when we switch to a different month/year in our data
+            previousDateEntry = firstRow['date'][0:2] + "/"+ firstRow['date'][6:]
+            currentDateEntry = previousDateEntry
+            #add the data to our accumulation dictionaries based on the law_cat_cd from the first row parsed
+            if firstRow['law_cat_cd'] == "FELONY":
+                felonyAccumulation[firstRow['zipcode']] = 1
+            elif firstRow['law_cat_cd'] == "MISDEMEANOR":
+                misdemeanorAccumulation[firstRow['zipcode']] = 1
+            elif firstRow['law_cat_cd'] == "VIOLATION":
+                violationAccumulation[firstRow['zipcode']] = 1
+            else:
+                print("MISSING DATA!")
+            outPutCSV = csv.writer(outfile,delimiter = ',')
+            outPutCSV.writerow(['date','zipcode','FELONIES', 'MISDEMEANORS', 'VIOLATIONS'])
                 
+            for row in complaintData:
+                currentDateEntry = row['date'][0:2] + "/" + row['date'][6:]
+
+                #if previousDateEntry not equal to currentDateEntry then we are in a new month
+                if previousDateEntry != currentDateEntry:
+                    #write to the output CSV file, all our values in our dictionary
+                    keysInFelony = list(felonyAccumulation.keys())
+                    #write all the zipcodes found in the felony dictionary
+                    for keyF in keysInFelony:
+                        zipCode = keyF
+                        felonyNumber = felonyAccumulation.pop(keyF)
+                        if keyF in misdemeanorAccumulation:
+                            misdemeanorNumber = misdemeanorAccumulation.pop(keyF)
+                        else:
+                            misdemeanorNumber = 0
+                        if keyF in violationAccumulation:
+                            violationNumber = violationAccumulation.pop(keyF)
+                        else:
+                            violationNumber = 0
+                        outPutCSV.writerow([previousDateEntry,zipCode,felonyNumber,misdemeanorNumber,violationNumber])
+                    #there may still be some zipcodes in misdemeanor and violation that were not a part of the felony zipcodes
+                    keysInMisdemeanor = list(misdemeanorAccumulation.keys())
+                    for keyD in keysInMisdemeanor:
+                        zipCode = keyD
+                        felonyNumber = 0
+                        misdemeanorNumber = misdemeanorAccumulation.pop(keyD)
+                        if keyD in violationAccumulation:
+                            violationNumber = violationAccumulation.pop(keyD)
+                        else:
+                            violationNumber = 0
+                        outPutCSV.writerow([previousDateEntry, zipCode, felonyNumber, misdemeanorNumber,violationNumber])
+                    #there may still be zome zipcodes in violation that were not a part of the previous two
+                    keysInViolation = list(violationAccumulation.keys())
+                    for keyV in keysInViolation:
+                        zipCode = keyV
+                        felonyNumber = 0
+                        misdemeanorNumber = 0
+                        violationNumber = violationAccumulation.pop(keyV)
+                        outPutCSV.writerow([previousDateEntry, zipCode, felonyNumber, misdemeanorNumber, violationNumber])
+                    #clear all dictionaries, however they should all be empty at this point
+                    felonyAccumulation.clear()
+                    misdemeanorAccumulation.clear()
+                    violationAccumulation.clear()
+                #add our data into the accumulated dictionaries based on law_cat_id
+                if row['law_cat_cd'] == "FELONY":
+                    if row['zipcode'] in felonyAccumulation:
+                        felonyAccumulation[row['zipcode']] += 1
+                    else:
+                        felonyAccumulation[row['zipcode']] = 1
+                elif row['law_cat_cd'] == "MISDEMEANOR":
+                    if row['zipcode'] in misdemeanorAccumulation:
+                        misdemeanorAccumulation[row['zipcode']] += 1
+                    else:
+                        misdemeanorAccumulation[row['zipcode']] = 1
+                elif row['law_cat_cd'] == "VIOLATION":
+                    if row['zipcode'] in violationAccumulation:
+                        violationAccumulation[row['zipcode']] += 1
+                    else:
+                        violationAccumulation[row['zipcode']] = 1
+                previousDateEntry = currentDateEntry
+            #need to write the last set of month into output
+            keysInFelony = list(felonyAccumulation.keys())
+            for keyF in keysInFelony:
+                zipCode = keyF
+                felonyNumber = felonyAccumulation.pop(keyF)
+                if keyF in misdemeanorAccumulation:
+                    misdemeanorNumber = misdemeanorAccumulation.pop(keyF)
+                else:
+                    misdemeanorNumber = 0
+                if keyF in violationAccumulation:
+                    violationNumber = violationAccumulation.pop(keyF)
+                else:
+                    violationNumber = 0
+                outPutCSV.writerow([previousDateEntry,zipCode,felonyNumber,misdemeanorNumber,violationNumber])
+            #there may still be some zipcodes in misdemeanor and violation that were not a part of the felony zipcodes
+            keysInMisdemeanor = list(misdemeanorAccumulation.keys())
+            for keyD in keysInMisdemeanor:
+                zipCode = keyD
+                felonyNumber = 0
+                misdemeanorNumber = misdemeanorAccumulation.pop(keyD)
+                if keyD in violationAccumulation:
+                    violationNumber = violationAccumulation.pop(keyD)
+                else:
+                    violationNumber = 0
+                outPutCSV.writerow([previousDateEntry, zipCode, felonyNumber, misdemeanorNumber,violationNumber])
+            #there may still be zome zipcodes in violation that were not a part of the previous two
+            keysInViolation = list(violationAccumulation.keys())
+            for keyV in keysInViolation:
+                zipCode = keyV
+                felonyNumber = 0
+                misdemeanorNumber = 0
+                violationNumber = violationAccumulation.pop(keyV)
+                outPutCSV.writerow([previousDateEntry, zipCode, felonyNumber, misdemeanorNumber, violationNumber])                    
+            
+                
+            
+            
+                          
             
             
             
