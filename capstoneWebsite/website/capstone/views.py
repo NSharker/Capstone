14941ad4f40f5dff08b5f94351794c65cbf37d25
Ira Lee <ilee2914@gmail.com>
2017-05-12 11:02:14 -0700

    MORE STUFFS
    got ajax to work
    legend added
    data box added with clicking on data

--- capstoneWebsite/website/capstone/views.py
+++ capstoneWebsite/website/capstone/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from pymongo import MongoClient
-
+from .forms import PostForm
+from django.http import HttpResponse
+import json
 # Create your views here.
 def index(request):
 	client = MongoClient('127.0.0.1', 27017)
 	db = client['househunter']
 	collection = db.historicaldata
-	data = list(collection.find({'date_enc':0}, {'_id':0}))
+	data = []
+	form = 'Welcome'
+	for i in range(0, 72):
+		data.append(list(collection.find({'date_enc':i}, {'_id':0})))
+	if request.method == 'POST':
+		months = request.POST.get('m')
+		years = request.POST.get('y')
+		response_data = {}
+		
+		response_data['m'] = months;
+		response_data['y'] = years;
+		
+		return HttpResponse(
+			json.dumps(response_data),
+			content_type="application/json"
+		)	
 	return render(request, 'capstone/home.html', {'data':data})
