1bb2fdb6fef514e05e7e576d96c717dbab0c5a99
Ira Lee <ilee2914@gmail.com>
2017-05-12 20:53:04 -0700

    oh bois
    machine learning added in
    website redesign??

--- capstoneWebsite/website/capstone/views.py
+++ capstoneWebsite/website/capstone/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 from pymongo import MongoClient
-from .forms import PostForm
 from django.http import HttpResponse
+from . import machinelearning as ml
 import json
+
 # Create your views here.
 def index(request):
 	client = MongoClient('127.0.0.1', 27017)
 	db = client['househunter']
-	collection = db.historicaldata
 	data = []
-	form = 'Welcome'
+	collection = db.historicaldata
 	for i in range(0, 72):
 		data.append(list(collection.find({'date_enc':i}, {'_id':0})))
 	if request.method == 'POST':
 		months = request.POST.get('m')
 		years = request.POST.get('y')
-		response_data = {}
+		zip = request.POST.get('z')
+		#response_data = {}
 		
-		response_data['m'] = months;
-		response_data['y'] = years;
+		#response_data['m'] = months;
+		#response_data['y'] = years;
 		
+		x = ml.predictHousingPrice(int(months), int(years), int(zip))
 		return HttpResponse(
-			json.dumps(response_data),
+			json.dumps(x),
 			content_type="application/json"
 		)	
 	return render(request, 'capstone/home.html', {'data':data})
